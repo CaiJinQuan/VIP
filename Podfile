fb92e8c258b72fe03d53e9a3c5a192d0ab5c9605
zhangjikuan <jikuan.chang@gmail.com>
2017-12-04 16:25:54 +0800

    2.1

--- Podfile
+++ Podfile
@@ -1,7 +1,7 @@
 # Uncomment this line to define a global platform for your project
 # platform :ios, '9.0'
 
-source 'https://github.com/CaiJinQuan/JKConfigPrivatePod.git'
+source 'https://github.com/CaiJinQuan/JKConfigPrivatePods.git'
 source 'https://github.com/CocoaPods/Specs.git'
 
 target 'VIP' do
--- upload.sh
+++ upload.sh
@@ -17,5 +17,5 @@ git add .
 git commit -am ${NewVersionNumber}
 git tag ${NewVersionNumber}
 git push origin master --tags
-cd ~/.cocoapods/repos/JKConfigPrivatePod && git pull origin master && cd - && pod repo push JKConfigPrivatePod VIP.podspec --verbose --allow-warnings --use-libraries
+cd ~/.cocoapods/repos/JKConfigPrivatePods && git pull origin master && cd - && pod repo push JKConfigPrivatePods VIP.podspec --verbose --allow-warnings --use-libraries
 
